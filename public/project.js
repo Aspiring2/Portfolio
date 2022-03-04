2e2f4af057f6b2451c58692d886714c7281511de
Daniil <daniil_rus96@bk.ru>
2022-03-04 12:17:58 +0300

    first commit

---
+++ public/project.js
@@ -0,0 +1,20 @@
+let projects = [
+    {
+        link:"https://github.com/Aspiring2/Weather.git",
+        name: "Weather API project",
+        tags: "#javascript",
+        image: "WeatherAPI.png",
+    },
+    {
+        link:"https://daniils-business-starter-d0b41e.webflow.io/",
+        name: "Webflow project",
+        tags: "#HTML",
+        image: "webflow.png",
+    },
+    {
+        link:"https://github.com/Aspiring2/My-JS.git",
+        name: "React project",
+        tags: "#React",
+        image: "react.png",
+    },
+]
